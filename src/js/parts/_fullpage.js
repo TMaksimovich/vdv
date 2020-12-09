bf8c5eab9f9a490b28e45468e2b23a3be273f5b1
tatiana maksimovich <tatianamaksimovich@gmail.com>
2020-12-09 19:06:06 +0300

    fix

--- src/js/parts/_fullpage.js
+++ src/js/parts/_fullpage.js
@@ -3,14 +3,29 @@ $(document).ready(function() {
       'autoScrolling': true,
 	    'scrollHorizontally': true,
       'slidesNavigation': true,
-	    'fixedElements': 'header, .footer-social',
+      'fixedElements': 'header, .footer-social',
+      'responsiveWidth': 400,
+      'scrollOverflow':true,
+      // 'responsiveHeight': 800,
+      //'responsiveSlides': true,
 
 	    onSlideLeave: function( section, origin, destination, direction){
-			var leavingSlide = origin.index;
+			  var leavingSlide = origin.index;
 
-			$('.fp-slide').removeClass('animateClose');
-      $('.fp-slide').eq(leavingSlide).addClass('animateClose');
-		}
+			  $('.fp-slide').removeClass('animateClose');
+        $('.fp-slide').eq(leavingSlide).addClass('animateClose');
+      },
+      afterResize: function() {
+        setTimeout(function(){
+          $('.front-page').css("height", "100vh");
+        }, 100);
+      }
 	})
+});
 
-})
+window.onpageshow = function(evt) {
+  if (evt.persisted) {
+      document.body.style.display = "none";
+      window.location.reload();
+  }
+};
